3ce3a07cd5a310705879f4e93f18e01299695918
Siketyan <siketyan@sikeserver.com>
2018-05-19 18:20:21 +0900

    Change assembly informations

--- Twimager/Properties/AssemblyInfo.cs
+++ Twimager/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("Twimager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Download images from listed Twitter accounts, and keep them latest.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Siketyan")]
 [assembly: AssemblyProduct("Twimager")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Siketyan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
