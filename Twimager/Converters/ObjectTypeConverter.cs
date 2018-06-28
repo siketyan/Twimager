9552e45d6e880cac7bf660edc1a7b1310eab0c4e
Siketyan <siketyan@sikeserver.com>
2018-06-28 17:32:32 +0900

    Add object type selection in ListBox

--- Twimager/Converters/ObjectTypeConverter.cs
+++ Twimager/Converters/ObjectTypeConverter.cs
@@ -8,7 +8,7 @@ namespace Twimager.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value.GetType().Name;
+            return value?.GetType().Name;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
