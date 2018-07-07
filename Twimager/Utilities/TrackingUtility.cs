eda53f8fc46ab66164775ef4ba5c923c60c298ee
Siketyan <siketyan@sikeserver.com>
2018-07-07 10:41:54 +0900

    Fix IsCompleted variable won't be reset

--- Twimager/Utilities/TrackingUtility.cs
+++ Twimager/Utilities/TrackingUtility.cs
@@ -6,6 +6,7 @@ namespace Twimager.Utilities
     {
         public static void Reset(this ITracking tracking)
         {
+            tracking.IsCompleted = false;
             tracking.Oldest = tracking.Latest = null;
             App.GetCurrent().Config.Save();
         }
